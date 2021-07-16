31bb3977f4a64e77617856c325f19c06321b36c3
umut deniz <umutdeniz609@gmail.com>
2021-07-17 00:45:17 +0300

    upload files

--- r.py
+++ r.py
@@ -1,25 +1,27 @@
 
-#pip install --upgrade google-cloud-storage
+# pip install --upgrade google-cloud-storage
+
+# download certain file
 from google.cloud import storage
 import os
 from pathlib import Path
 import nltk
 nltk.download('punkt')
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "betatests-bad323259eb9.json"
-storage_client = storage.Client("[BetaTests]")
-bucket = storage_client.get_bucket('betatests.appspot.com')
-blob = bucket.blob("chatbot/chatbotmodel.h5")
-blob.download_to_filename("chatbotmodel.h5")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "your credential"
+storage_client = storage.Client("[project name]")
+bucket = storage_client.get_bucket('bucketname')
+blob = bucket.blob("path/to/file")
+blob.download_to_filename("filename")
 
 
-#Download dir
-bucket = storage.Bucket(storage_client, "betatests.appspot.com", user_project="BetaTests")
+# Download directory
+bucket = storage.Bucket(storage_client, "bucketname", user_project="project")
 all_blobs = list(storage_client.list_blobs(bucket))
 
 for blob in all_blobs:
     if blob.name.endswith("/"):
         continue
-    if blob.name.startswith("chatbot/nltk_data/"):
+    if blob.name.startswith("path/to/file"):
         file_split = blob.name.split("/")
         directory = "/".join(file_split[0:-1])
         Path(directory).mkdir(parents=True, exist_ok=True)
